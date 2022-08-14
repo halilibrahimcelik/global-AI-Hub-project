efb635999a24a51e4a8dc0794d9477f4f2256e94
halilibrahimcelik <halil.celik139@gmail.com>
2022-08-14 15:32:45 +0300

    Added Home Component.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import styles from "./Home.module.css";
+import image from "../../assets/AI logo.jpg.jpg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className={styles.container}>
+      <header className={styles.header}>
+        <h1>Welcome to Global AI Hub </h1>
+      </header>
+      <main className={styles.info}>
+        <p className={styles.text}>
+          Global AI Hub is a Swiss-based leading global AI community for
+          everyone with an interest in AI. Join our global and local community
+          hubs to expand your network, learn from thought leaders and connect
+          with people around the world who are also passionate about AI.
+        </p>
+        <img className={styles.image} src={image} alt="AI-logo" />
+      </main>
+    </section>
+  );
 };
 
 export default Home;
