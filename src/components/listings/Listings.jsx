017f04aa958faf95173eb5c8ab4bac6d934c58cf
halilibrahimcelik <halil.celik139@gmail.com>
2022-08-14 18:32:52 +0300

    fixed  a styling bug

--- src/components/listings/Listings.jsx
+++ src/components/listings/Listings.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Listings.module.css";
 import defaultLogo from "../../assets/default-logo.jpg";
 
 const Listings = (props) => {
-  const { card_image, description, link, title, categories } = props;
-  console.log(props);
+  const {
+    card_image,
+    description,
+    link,
+    title,
+    categories,
+    setActive,
+    active,
+    id,
+
+    setKeyId,
+    keyId,
+  } = props;
+
   return (
     <li className={styles["list-item"]}>
       <img
+        onClick={() => {
+          setKeyId(id);
+          setActive(true);
+        }}
         className={styles.image}
         referrerpolicy="no-referrer"
         src={card_image ? card_image : defaultLogo}
         alt={title}
       />
+      {active && id === keyId && (
+        <div className={styles["pop-up-container"]}>{description} </div>
+      )}
     </li>
   );
 };
--- src/pages/myCourses/Mycourses.jsx
+++ src/pages/myCourses/Mycourses.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Listings from "../../components/listings/Listings";
 import { useAuthContext } from "../../hook/AuthContext";
 import styles from "./MyCourse.module.css";
 const MyCourses = () => {
   const { myCourses, allCourses } = useAuthContext();
+  const [active, setActive] = useState(false);
+  const [keyId, setKeyId] = useState("");
   console.log(myCourses);
 
   return (
@@ -11,9 +13,29 @@ const MyCourses = () => {
       <ul className={styles.list}>
         {myCourses?.map((course) => {
           const { id } = course;
-          return <Listings key={id} {...course} />;
+          return (
+            <Listings
+              key={id}
+              {...course}
+              setActive={setActive}
+              active={active}
+              setKeyId={setKeyId}
+              keyId={keyId}
+            />
+          );
         })}
       </ul>
+      <div
+        onClick={() => {
+          setKeyId("");
+          setActive(false);
+        }}
+        className={
+          active
+            ? styles["pop-up-container"] + " " + styles.active
+            : styles["pop-up-container"]
+        }
+      ></div>
     </section>
   );
 };
